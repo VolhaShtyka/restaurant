2fa9300e19b4906d34a9008dd0bcdf2dc6a762ea
VolhaShtyka <3470097@gmail.com>
2016-10-26 08:01:48 +0300

    change POMs

--- dao/src/main/java/com.shtyka/dao/daoIlml/UserDaoImpl.java
+++ dao/src/main/java/com.shtyka/dao/daoIlml/UserDaoImpl.java
@@ -22,96 +22,100 @@ public class UserDaoImpl implements UserDao {
     private static final String SQL_DELETE_CLIENT = ManagerSQL.getProperty("SQL_DELETE_CLIENT");
 
 
-   public List<User> update(User entity) throws SQLException {
-       List<User> clients = new ArrayList<User>();
-       JdbcTemplate jc = new JdbcTemplate();
-       ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
-       while (resultSet.next()) {
-           User client = new User();
-           client.setName(resultSet.getString("client_name"));
-           client.setTableNumber(resultSet.getInt("table_number"));
-           clients.add(client);
-       }
-       resultSet.close();
-       jc.closeJdbcTemplate();
-       return clients;
+    public List<User> update(User entity) throws SQLException {
+        List<User> users = new ArrayList<User>();
+        JdbcTemplate jc = new JdbcTemplate();
+        ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
+        while (resultSet.next()) {
+            User user = new User();
+            user.setName(resultSet.getString("client_name"));
+            user.setTableNumber(resultSet.getInt("table_number"));
+            users.add(user);
+        }
+        resultSet.close();
+        jc.closeJdbcTemplate();
+        return users;
     }
 
     public User findByLogin(String login) throws SQLException {
         JdbcTemplate jc = new JdbcTemplate();
         User user = new User();
         ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
-            while (resultSet.next()) {
+        while (resultSet.next()) {
 
-                if (resultSet.getString("client_name").equals(login)) {
-                    user.setName(resultSet.getString("client_name"));
-                    user.setTableNumber(resultSet.getInt("table_number"));
-                    user.setId(resultSet.getInt("client_id"));
-                }
+            if (resultSet.getString("client_name").equals(login)) {
+                user.setName(resultSet.getString("client_name"));
+                user.setTableNumber(resultSet.getInt("table_number"));
+                user.setId(resultSet.getInt("client_id"));
             }
-            resultSet.close();
+        }
+        resultSet.close();
         jc.closeJdbcTemplate();
         return user;
     }
+
     public List<User> findAll() throws SQLException {
-        List<User> clients = new ArrayList< User>();
+        List<User> clients = new ArrayList<User>();
         JdbcTemplate jc = new JdbcTemplate();
         ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
-            while (resultSet.next()) {
-                if(resultSet.getInt("role_id") == 2){
-                    User client = new User();
-                    client.setId(resultSet.getInt("client_id"));
-                    client.setName(resultSet.getString("client_name"));
-                    client.setTableNumber(resultSet.getInt("table_number"));
-                    clients.add(client);
-                }
+        while (resultSet.next()) {
+            if (resultSet.getInt("role_id") == 2) {
+                User client = new User();
+                client.setId(resultSet.getInt("client_id"));
+                client.setName(resultSet.getString("client_name"));
+                client.setTableNumber(resultSet.getInt("table_number"));
+                clients.add(client);
             }
+        }
         resultSet.close();
         jc.closeJdbcTemplate();
         return clients;
     }
+
     public User findById(int id) throws SQLException {
-        User client= new User();
         JdbcTemplate jc = new JdbcTemplate();
-            ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
-            while (resultSet.next()) {
-                if (resultSet.getInt("client_id") == id) {
-                    client.setId(resultSet.getInt("cient_id"));
-                    client.setName(resultSet.getString("client_name"));
-                    client.setTableNumber(resultSet.getInt("table_number"));
-                }
+        User user = new User();
+        ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_CLIENTS);
+        while (resultSet.next()) {
+            if (id == resultSet.getInt("client_id")) {
+                user.setName(resultSet.getString("client_name"));
+                user.setTableNumber(resultSet.getInt("table_number"));
+                user.setId(resultSet.getInt("client_id"));
             }
-            resultSet.close();
-        JdbcTemplate.closeJdbcTemplate();
-        return client;
-    }
-    public int countOrder(User user) throws SQLException {
-        int sum = 0;
-        JdbcTemplate jc = new JdbcTemplate();
-            ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_ORDERS + user.getId());
-            while (resultSet.next()) {
-                if (!resultSet.getString("status_order").equals("CHEKED")) {
-                    sum += (resultSet.getInt("price"));
-                }
             }
             resultSet.close();
             JdbcTemplate.closeJdbcTemplate();
+            return user;
+        }
+
+    public int countOrder(User user) throws SQLException {
+        int sum = 0;
+        JdbcTemplate jc = new JdbcTemplate();
+        ResultSet resultSet = JdbcTemplate.st.executeQuery(SQL_SELECT_ALL_ORDERS + user.getId());
+        while (resultSet.next()) {
+            if (!resultSet.getString("status_order").equals("CHEKED")) {
+                sum += (resultSet.getInt("price"));
+            }
+        }
+        resultSet.close();
+        JdbcTemplate.closeJdbcTemplate();
         return sum;
     }
+
     public boolean delete(int id) {
         Connection cn = null;
         Statement st = null;
-        try{
+        try {
             cn = DataSource.getInstance().getConnection();
             st = cn.createStatement();
-            st.executeUpdate(SQL_DELETE_CLIENT+id);
-        }catch(SQLException e){
+            st.executeUpdate(SQL_DELETE_CLIENT + id);
+        } catch (SQLException e) {
             System.out.println("Request or table failed. Client not pay for order and clean.");
         } catch (IOException e) {
             System.out.println("IOException e: ClientDAO");
         } catch (PropertyVetoException e) {
             System.out.println("PropertyVetoException e: ClientDAO");
-        }finally{
+        } finally {
 
             try {
                 st.close();
